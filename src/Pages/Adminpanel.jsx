48a9c0d460849291944dd38455334533427a8543
jankquin <jankquin@gmail.com>
2022-04-18 19:46:55 +0700

    minor

--- src/Pages/Adminpanel.jsx
+++ src/Pages/Adminpanel.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import { useState } from "react";
 
 const AdminPanel = () => {
-    // Create Function
-    
-    const css = `
-        body {
-            overflow: hidden;
-        }
-    `
-
-
     return (
         <>
-            <div className="mb-10">
-                <button className="hover:bg-neutral-900 focus:bg-neutral-900 bg-neutral-700 text-white font-medium text-xs uppercase rounded mx-auto px-6 py-2.5 mt-3" type="button">Add Post</button>
-            </div>
+            <p className='text-white'>asdasdasd</p>
 
         </>
     )
