ede64dc41a907792396d1899f61ab3c783b4e16c
jankquin <jankquin@gmail.com>
2022-04-19 20:22:13 +0700

    minor

--- src/Pages/PostDetail.jsx
+++ src/Pages/PostDetail.jsx
@@ -1,25 +1,16 @@
-import { async } from '@firebase/util';
 import React from 'react'
-import { useState, useEffect } from "react";
 import { useParams, Link } from 'react-router-dom'
 import Tb_Post from '../Firebase/Tb_Post'
 import Tb_Similar from '../Firebase/Tb_Similar'
 
 const PostDetail = () => {
-    const [UseLoadMore, setUseLoadMore] = useState(4);
     const {id} = useParams();
     
     const DataPost          = Tb_Post().UsePost;  
-    const DataSimilar       = Tb_Similar()._Post;  
-    const GetPost           = DataPost.slice(0, UseLoadMore);
+    const DataSimilar       = Tb_Similar()._Post;
     const GetPostDetail     = DataPost.find(doc => doc.id == id);
     const SetSimilar        = DataSimilar.slice(0, -4);
     const GetSimilar        = DataPost.filter(doc => doc.title.toLowerCase().includes(SetSimilar.toString().toLowerCase()));
-
-    const LoadMoreBtn       = ()  => {
-        const GetLoadMore   = UseLoadMore + 4;
-        setUseLoadMore(GetLoadMore)
-    }
         
     const OnTop             = () => {
         document.body.scrollTop = 0;
@@ -30,11 +21,10 @@ const PostDetail = () => {
 
     return (
         <>
-
             {GetPostDetail != null && (
                 <div className="mb-10">   
                     <div className="bg-neutral-700 rounded-sm h-80 mb-3">
-                        LINK EMBED
+                        <iframe className='w-full h-full' src={GetPostDetail.embed} scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
                     </div>
 
                     <div className="flex justify-between py-3">
@@ -49,12 +39,8 @@ const PostDetail = () => {
                             </div>
                         </div>
                         <div className="flex self-center">
-                            <button className="bg-neutral-700 hover:bg-neutral-900 self-center rounded-sm text-white text-xs font-medium uppercase px-3 py-2">
-                                Download
-                            </button>
-                            <Link to={`/`} className="bg-neutral-700 hover:bg-neutral-900 self-center rounded-sm text-white text-xs font-medium uppercase px-3 py-2 ml-1">
-                                Back
-                            </Link>
+                            <button className="bg-neutral-700 hover:bg-neutral-900 self-center rounded-sm text-white text-xs font-medium uppercase px-3 py-2">Download</button>
+                            <Link to={`/`} className="bg-neutral-700 hover:bg-neutral-900 self-center rounded-sm text-white text-xs font-medium uppercase px-3 py-2 ml-1">Back</Link>
                         </div>
                     </div>
 
@@ -82,29 +68,6 @@ const PostDetail = () => {
                         )
                     })}
                 </div>
-            )}
-
-            <h2 className="text-white font-medium mb-3">New Uploaded</h2>
-
-            <div className="grid grid-cols-4 gap-1 mb-10">
-                {GetPost.map(doc => {
-                    return (
-                        <a href={`/${doc.id}`} key={doc.id} className="group overflow-hidden w-full mb-3">    
-                            <img src="https://git-covers.pages.dev/images/victorian-maid-maria.jpg" className="rounded-sm" alt={doc.title} height="40"/> 
-                            <h3 className="whitespace-nowrap text-ellipsis overflow-hidden text-white text-sm font-thin px-2 py-1">{doc.title} Lorem ipsum dolor sit amet.</h3>
-                            <div className="flex justify-center pb-3">
-                                <i className="bi-eye-fill text-white text-sm self-center mr-3"></i>
-                                <div className="text-white text-xs self-center font-light">{doc.id}</div>
-                            </div>
-                        </a>
-                    )
-                })}
-            </div>
-
-            {UseLoadMore < DataPost.length && (
-                <button className="hover:bg-neutral-700 active:bg-neutral-900 text-white font-medium block text-xs uppercase rounded-sm mx-auto px-6 py-2.5" onClick={LoadMoreBtn}>
-                    <span>Load More</span>
-                </button>
             )}
         </>
     )
