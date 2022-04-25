1a02b558192b49e41c1a4c3e85326e1faf165daf
jankquin <jankquin@gmail.com>
2022-04-25 08:14:36 +0700

    minor

--- src/Pages/PagePost.jsx
+++ src/Pages/PagePost.jsx
@@ -10,15 +10,6 @@ const PagePost = () => {
     const DataPost = Tb_Post().UsePost;
     const DataRecomended = Tb_Recomended().UseRecomended;
 
-    const LoadMoreBtn = () => {
-        const GetLoadMore = LoadMore + 4;
-        setLoadMore(GetLoadMore);
-    }
-
-    const sliceData = DataPost.slice(0, LoadMore);
-
-
-
     const CarouselSlice = DataRecomended.slice(Carousel[0], Carousel[1])
     
     const CarouselBtn = (event) => {
@@ -43,37 +34,43 @@ const PagePost = () => {
         }
     }
     
+    const LoadMoreBtn = () => {
+        const GetLoadMore = LoadMore + 4;
+        setLoadMore(GetLoadMore);
+    }
+
+    const SliceData = DataPost.slice(0, LoadMore);
+
 
     return (
         <>
             <div className="container mx-auto mt-28 px-3 py-5">
                 <div className="flex justify-center mb-10">
                     <div className="lg:basis-5/12 md:basis-8/12 basis-full">
-                        
-                        {/* {Slice.Id} */}
 
                         {CarouselSlice.map(doc => {
                             return (
-                                <div key={doc.Id}>
-                                    <div key={doc.Id} className="group hover:drop-shadow-lg relative block w-full rounded-sm overflow-hidden h-48 mb-10">
-                                        <div className="bg-gradient-to-r from-neutral-700 via-neutral-700 absolute flex h-full w-full top-0 z-10">
-                                            <div className="self-end w-2/4 pl-5 pb-5 animate-fadeIn">
-                                                <div className="bg-[url('https://git-covers.pages.dev/images/victorian-maid-maria.jpg')] bg-no-repeat bg-cover bg-center border-2 rounded-full drop-shadow-sm w-12 h-12 mb-3"></div>
-                                                <h2 className="whitespace-nowrap text-ellipsis text-left overflow-hidden text-slate-200 md:text-xl text-sm font-medium">{doc.Title}</h2>
-                                                <p className="text-slate-200 text-xs font-thin text-left">{doc.View} - {doc.Id}</p>
-                                            </div>
+                                <div key={doc.Id} className="relative mb-10">
+                                    <div className="group hover:drop-shadow-lg relative block rounded-sm overflow-hidden w-full h-48 animate-fadeIn">
+                                        <div className="bg-gradient-to-r from-neutral-700 via-neutral-700 absolute left-0 top-0 h-full w-full z-10"></div>
+                                        <div className="bg-[url('https://git-covers.pages.dev/images/victorian-maid-maria.jpg')] group-hover:scale-125 bg-cover bg-top scale-110 duration-500 absolute right-0 top-0 h-full w-2/4 z-0"></div>
+                                    </div>
 
-                                            <div className='flex self-center justify-between w-2/4 p-3'>
-                                                <button className='bg-neutral-200/50 rounded-full w-8 h-8' onClick={(event) => CarouselBtn('Next')}>
-                                                    <i className='bi-chevron-left'></i>
-                                                </button>
-                                                <button className='bg-neutral-200/50 rounded-full w-8 h-8' onClick={(event) => CarouselBtn('Prev')}>
-                                                    <i className='bi-chevron-right'></i>
-                                                </button>
-                                            </div>
+                                    <div className='absolute flex h-full w-2/4 top-0 z-20 animate-fadeIn'>
+                                        <div className='self-end pl-8 pb-5'>
+                                            <div className="bg-[url('https://git-covers.pages.dev/images/victorian-maid-maria.jpg')] bg-no-repeat bg-cover bg-center border-2 rounded-full drop-shadow-sm w-12 h-12 mb-3"></div>
+                                            <h2 className="whitespace-nowrap text-ellipsis text-left overflow-hidden text-slate-200 md:text-xl text-sm font-medium">{doc.Title}</h2>
+                                            <p className="text-slate-200 text-xs font-thin text-left">{doc.View} - {doc.Id}</p>
                                         </div>
-
-                                        <div className="bg-[url('https://git-covers.pages.dev/images/victorian-maid-maria.jpg')] bg-cover bg-top group-hover:scale-125 scale-110 duration-500 absolute right-0 h-full top-0 w-2/4 z-0"></div>
+                                    </div>
+                                   
+                                    <div className='absolute flex justify-between h-full w-full top-0 z-20'>
+                                        <button className='bg-neutral-100 self-center rounded-full shadow-lg w-8 h-8 -ml-4' onClick={(event) => CarouselBtn('Next')}>
+                                            <i className='bi-chevron-left text-indigo-500'></i>
+                                        </button>
+                                        <button className='bg-neutral-100 self-center rounded-full shadow-lg w-8 h-8 -mr-4' onClick={(event) => CarouselBtn('Prev')}>
+                                            <i className='bi-chevron-right text-indigo-500'></i>
+                                        </button>
                                     </div>
                                 </div>
                             )
@@ -87,7 +84,7 @@ const PagePost = () => {
                         </div>
 
                         <div className="grid grid-cols-4 gap-1 bg-white shadow-lg rounded-sm overflow-hidden p-3 mb-10">
-                            {sliceData.map(doc => {
+                            {SliceData.map(doc => {
                                 return (
                                     <Link to={`/${doc.Id}`} key={doc.Id} className="group rounded-sm overflow-hidden w-full mb-3">    
                                         <img src="https://git-covers.pages.dev/images/victorian-maid-maria.jpg" className="group-hover:opacity-90 rounded-sm" alt={doc.Title} height="40"/> 
