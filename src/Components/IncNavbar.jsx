3fdfb78be0fffb746ee7190848fb53753ceeab88
jankquin <jankquin@gmail.com>
2022-05-31 21:25:25 +0700

    minor

--- src/Components/IncNavbar.jsx
+++ src/Components/IncNavbar.jsx
@@ -1,13 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Assets/Logo.svg";
 import Tb_Post from "../Firebase/Tb_Post";
+// import useDarkMode from '../Components/useDarkMode';
 
 const IncNavbar = () => {
+    const [DarkMode, setDarkMode] = useState(false);
     const [Modal, setModal] = useState({Alert: false})
     const [Search, setSearch] = useState(""); 
     const [LabelSearch, setLabelSearch] = useState(true);
     const [Found, setFound] = useState([]);   
+
+    useEffect(() => {
+        if (localStorage.getItem('theme') === 'dark') {
+            setDarkMode(true);
+            document.querySelector('html').classList.add('dark');
+        } else {
+            setDarkMode(false);
+            document.querySelector('html').classList.remove('dark');
+        }
+    }, []);
+
+    function DarkModBtn() {
+        if (localStorage.getItem('theme') === 'dark') {
+            document.querySelector('html').classList.remove('dark');
+            setDarkMode(false);
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.querySelector('html').classList.add('dark');
+            setDarkMode(true);
+            localStorage.setItem('theme', 'dark');
+        }
+    }
     
     let DataPost = Tb_Post().UsePost;
 
@@ -28,41 +52,45 @@ const IncNavbar = () => {
         setLabelSearch(true);
     }
 
+
     return (
         <>
-            <div className="bg-white dark:bg-slate-900 shadow fixed w-full top-0 p-3 z-50">
+            <div className="dark:bg-zinc-900 bg-white shadow fixed w-full top-0 p-3 z-50">
                 <div className="container mx-auto">
                     <div className="flex justify-center">
-                        <div className="lg:w-3/12 md:w-2/12 md:bg-transparent bg-slate-100 flex">
+                        <div className="lg:w-3/12 md:w-2/12 dark:md:bg-transparent dark:bg-zinc-700 md:bg-transparent bg-zinc-300 flex">
                             <Link to={`/`}className="flex px-3">
-                                <img src={Logo} alt="Logo" width={'26'} />
+                                <img src={Logo} alt="Logo" className="md:w-[24px] w-[46px]" />
                             </Link>
                         </div>
                         
                         <div className="lg:w-5/12 md:w-8/12 w-full">
-                            <form className="dark:bg-slate-700 bg-slate-100 relative rounded">
+                            <form className="dark:bg-zinc-700 bg-zinc-300 relative md:rounded">
                                 {LabelSearch ? 
-                                    <div className="absolute flex text-slate-500 text-sm font-medium uppercase h-full w-full">
+                                    <div className="absolute flex text-zinc-500 text-sm font-medium uppercase h-full w-full">
                                         <div className="mx-auto self-center">
                                             <i className="bi-search mr-3"></i> Search
                                         </div>
                                     </div>
                                     : 
-                                    <div className="absolute flex text-slate-500 h-full w-full z-30" onClick={ModalSearchClose}>
+                                    <div className="absolute flex text-zinc-500 h-full w-full z-30" onClick={ModalSearchClose}>
                                         <button className="ml-auto self-center p-3">
                                             <i className="bi-x"></i>
                                         </button>
                                     </div>
                                 }
-                                <input className="bg-transparent text-slate-500 rounded text-sm relative outline-none w-full z-10 p-3" type="text" value={Search} onChange={Filter}/>
+                                <input className="bg-transparent text-zinc-500 rounded text-sm relative outline-none w-full z-10 p-3" type="text" value={Search} onChange={Filter}/>
                             </form>
                         </div>
 
-                        <div className="lg:w-3/12 md:w-2/12 md:bg-transparent bg-slate-100 flex justify-end">
-                            <button className="flex dark:hover:bg-slate-700 text-slate-500 hover:bg-slate-200 rounded-full px-3 mr-5">
-                                <i className="bi-moon-stars-fill text-xl self-center"></i>
+                        <div className="lg:w-3/12 md:w-2/12 dark:md:bg-transparent dark:bg-zinc-700 md:bg-transparent bg-zinc-200 flex justify-end">
+                            <button className="flex dark:hover:bg-zinc-700 text-zinc-500 hover:bg-zinc-200 rounded-full px-3 mr-5" onClick={DarkModBtn}>
+                                {DarkMode ? 
+                                    <i className="bi-moon-stars-fill text-xl self-center"></i> :
+                                    <i className="bi-moon-stars text-xl self-center"></i>
+                                }
                             </button>
-                            <button className="flex dark:hover:bg-slate-700 text-slate-500 hover:bg-slate-200 rounded-full px-3" onClick={Modal.Alert == true ? (event) => setModal({Alert: false}) : (event) => setModal({Alert: true})}>
+                            <button className="flex dark:hover:bg-zinc-700 text-zinc-500 hover:bg-zinc-200 rounded-full px-3" onClick={Modal.Alert == true ? (event) => setModal({Alert: false}) : (event) => setModal({Alert: true})}>
                                 <i className="bi-grid-fill text-xl self-center"></i>
                             </button>
                         </div>
@@ -71,21 +99,21 @@ const IncNavbar = () => {
             </div>
       
             {Search.length !== 0 && (
-                <div className="bg-slate-600/80 fixed w-full h-full top-0 left-0 z-30 animate-fadeIn">
+                <div className="bg-zinc-900/90 fixed w-full h-full top-0 left-0 z-30 animate-fadeIn">
                     <div className="container md:px-0 px-3 py-2 mx-auto mt-16">
                         <div className="flex justify-center">
-                            <div className="lg:basis-5/12 md:basis-8/12 basis-full bg-white shadow-lg rounded-sm p-5 overflow-hidden block">
+                            <div className="lg:basis-5/12 md:basis-8/12 basis-full dark:bg-zinc-900 bg-white shadow-lg rounded-sm p-5 overflow-hidden block">
                                 {Found.length !== 0 ?
                                     <>
                                         {Found.slice(0, 5).map((doc, key) => {
                                             return (
-                                                <a href={`/${doc.Id}`} key={doc.Id} className="flex bg-slate-100 hover:bg-slate-200 hover:text-indigo-400 text-slate-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
+                                                <a href={`/${doc.Id}`} key={doc.Id} className="flex dark:bg-zinc-700 bg-zinc-100 hover:bg-zinc-200 hover:text-indigo-400 text-zinc-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
                                                     <div style={{ backgroundImage: `url(${doc.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full shadow self-center w-10 h-10 mr-3'></div> 
                                                     <span>
                                                         <div className="font-medium">{doc.Title}</div>
                                                         <div className="flex">
-                                                            <i className="bi-eye-fill group-hover:text-indigo-400 text-slate-400 self-center mr-3" />
-                                                            <div className="group-hover:text-indigo-400 text-slate-400 self-center text-xs">{doc.View}</div>
+                                                            <i className="bi-eye-fill group-hover:text-indigo-400 text-zinc-400 self-center mr-3" />
+                                                            <div className="group-hover:text-indigo-400 text-zinc-400 self-center text-xs">{doc.View}</div>
                                                         </div>
                                                     </span>
                                                 </a>    
@@ -94,10 +122,10 @@ const IncNavbar = () => {
                                     </> 
                                     :
                                     <>
-                                        <h2 className="text-slate-600 text-center font-medium">Not Found</h2>
+                                        <h2 className="text-zinc-600 text-center font-medium">Not Found</h2>
                                     </>
                                 }
-                                <div className="text-slate-400 text-xs mt-5">Powered by : Valonime</div>
+                                <div className="text-zinc-400 text-xs mt-5">Powered by : Valonime</div>
                             </div>
                         </div>
                     </div>
@@ -105,45 +133,45 @@ const IncNavbar = () => {
             )}
 
             {Modal.Alert == true && (
-                <div className="fixed bg-slate-600/80 flex top-0 left-0 w-full h-full z-40">
+                <div className="fixed bg-zinc-600/80 flex top-0 left-0 w-full h-full z-40">
                     <div className="bg-white lg:w-5/12 w-10/12 rounded self-center mx-auto p-5 animate-fadeIn">
                         <div className="flex justify-between mb-5">
-                            <button className="text-slate-600 text-xl font-medium" onClick={(event) => setModal({Alert: false})}>Valonime</button>
-                            <button className="hover:bg-slate-200 rounded-full px-2 py-1" onClick={(event) => setModal({Alert: false})}>
+                            <button className="text-zinc-600 text-xl font-medium" onClick={(event) => setModal({Alert: false})}>Valonime</button>
+                            <button className="hover:bg-zinc-200 rounded-full px-2 py-1" onClick={(event) => setModal({Alert: false})}>
                                 <i className="bi-x"/>
                             </button>
                         </div>
 
-                        <Link to="/" className="flex bg-slate-200/50 hover:text-indigo-400 text-slate-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
+                        <Link to="/" className="flex bg-zinc-100 hover:text-indigo-400 text-zinc-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
                             <i className="bi-star-fill text-2xl self-center mr-3"/>    
                             <span>
                                 <div className="font-medium">Recomended</div>
-                                <div className="group-hover:text-indigo-400 text-slate-400 text-xs">Most View</div>
+                                <div className="group-hover:text-indigo-400 text-zinc-400 text-xs">Most View</div>
                             </span>
                         </Link>
-                        <Link to="/more" className="flex bg-slate-200/50 hover:text-indigo-400 text-slate-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
+                        <Link to="/more" className="flex bg-zinc-100 hover:text-indigo-400 text-zinc-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
                             <i className="bi-grid-fill text-2xl self-center mr-3"/>    
                             <span>
                                 <div className="font-medium">Genre</div>
-                                <div className="group-hover:text-indigo-400 text-slate-400 text-xs">Choose your Genre</div>
+                                <div className="group-hover:text-indigo-400 text-zinc-400 text-xs">Choose your Genre</div>
                             </span>
                         </Link>
-                        <Link to="/" className="flex bg-slate-200/50 hover:text-indigo-400 text-slate-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
+                        <Link to="/" className="flex bg-zinc-100 hover:text-indigo-400 text-zinc-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
                             <i className="bi-star-fill text-2xl self-center mr-3"/>    
                             <span>
                                 <div className="font-medium">Recomended</div>
-                                <div className="group-hover:text-indigo-400 text-slate-400 text-xs">Most View</div>
+                                <div className="group-hover:text-indigo-400 text-zinc-400 text-xs">Most View</div>
                             </span>
                         </Link>
-                        <Link to="/more" className="flex bg-slate-200/50 hover:text-indigo-400 text-slate-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
+                        <Link to="/more" className="flex bg-zinc-100 hover:text-indigo-400 text-zinc-600 group rounded px-4 py-2 mb-1" onClick={(event) => setModal({Alert: false})}>
                             <i className="bi-grid-fill text-2xl self-center mr-3"/>    
                             <span>
                                 <div className="font-medium">Genre</div>
-                                <div className="group-hover:text-indigo-400 text-slate-400 text-xs">Choose your Genre</div>
+                                <div className="group-hover:text-indigo-400 text-zinc-400 text-xs">Choose your Genre</div>
                             </span>
                         </Link>
 
-                        <div className="text-slate-400 text-xs mt-5">Powered by : Valonime</div>
+                        <div className="text-zinc-400 text-xs mt-5">Powered by : Valonime</div>
                     </div>
                 </div>
             )}
