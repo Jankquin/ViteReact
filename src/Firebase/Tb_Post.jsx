00d5240aba83e7be5747949c3fbf1250b39dd3c2
jankquin <jankquin@gmail.com>
2022-06-13 21:46:13 +0700

    minor

--- src/Firebase/Tb_Post.jsx
+++ src/Firebase/Tb_Post.jsx
@@ -6,26 +6,30 @@ const Tb_Post = () => {
     const [UsePost, setUsePost] = useState([]); 
     const [UseMostView, setUseMostView] = useState([]); 
     const [UseRecomended, setUseRecomended] = useState([]); 
+    const [UseNewRelease, setUseNewRelease] = useState([]); 
 
     useEffect(() => {
-        const QPost           = query(collection(db, "Tb_Post"), orderBy("Id", "desc"));
-        const QMostView       = query(collection(db, "Tb_Post"), orderBy("View", "desc"), limit(8));
-        const QRecomended     = query(collection(db, "Tb_Post"), orderBy("Id", "desc"), limit(3));
+        const QuerryPost         = query(collection(db, "Tb_Post"), orderBy("Id", "desc"));
+        const QuerryMostView     = query(collection(db, "Tb_Post"), orderBy("View", "desc"), limit(8));
+        const QuerryRecomended   = query(collection(db, "Tb_Post"), orderBy("Id", "desc"), limit(3));
+        const QuerryNewRelease   = query(collection(db, "Tb_Post"), orderBy("Release", "desc"), limit(8));
         
-        const FirstLoadAsync  = async () => {       
-            const DPost       = await getDocs(QPost);     
-            const DMostView   = await getDocs(QMostView);     
-            const DRecomended = await getDocs(QRecomended);  
+        const FirstLoadAsync     = async () => {       
+            const DataPost       = await getDocs(QuerryPost);     
+            const DataMostView   = await getDocs(QuerryMostView);     
+            const DataRecomended = await getDocs(QuerryRecomended);  
+            const DataNewRelease = await getDocs(QuerryNewRelease);  
 
-            setUsePost(DPost.docs.map((doc) => ({ ...doc.data()})));
-            setUseMostView(DMostView.docs.map((doc) => ({ ...doc.data()})));
-            setUseRecomended(DRecomended.docs.map((doc) => ({ ...doc.data()})));
+            setUsePost(DataPost.docs.map((doc) => ({ ...doc.data()})));
+            setUseMostView(DataMostView.docs.map((doc) => ({ ...doc.data()})));
+            setUseRecomended(DataRecomended.docs.map((doc) => ({ ...doc.data()})));
+            setUseNewRelease(DataNewRelease.docs.map((doc) => ({ ...doc.data()})));
         };
 
         FirstLoadAsync();
     }, []);
 
-    return {UsePost, UseMostView, UseRecomended}
+    return {UsePost, UseMostView, UseRecomended, UseNewRelease}
 }
 
 export default Tb_Post;
--- src/Pages/PostDetail.jsx
+++ src/Pages/PostDetail.jsx
@@ -8,6 +8,7 @@ const PostDetail = () => {
     const {id} = useParams();
     
     const DataPost          = Tb_Post().UsePost;  
+    const DataNewRelease    = Tb_Post().UseNewRelease;
     const DataSimilar       = Tb_Similar().UseSimilar;
     const GetPostDetail     = DataPost.find(doc => doc.Id == id);
     const SetSimilar        = DataSimilar.slice(0, -4);
@@ -18,17 +19,36 @@ const PostDetail = () => {
         document.documentElement.scrollTop = 0;
     }
 
+    console.log(DataNewRelease)
+
     return (
         <>
-            <div className="container lg:w-11/12 mx-auto mt-20 lg:px-0 md:px-3">
+            <div className="container lg:w-11/12 mx-auto md:mt-20 mt-16 lg:px-0 md:px-3">
                 {GetPostDetail && (
                     <div className="dark:bg-zinc-800 bg-white shadow-lg md:flex block rounded overflow-hidden mb-5">
-                        <div className="rounded lg:w-9/12 md:w-8/12 lg:h-[30rem] h-[20rem] w-full"> 
+                        <div className="lg:w-9/12 md:w-8/12 lg:h-[30rem] h-[20rem] w-full rounded"> 
                             <iframe className='w-full h-full animate-fadeIn' src={GetPostDetail.Embed} scrolling="no" frameBorder="0" allowFullScreen={true}></iframe>
                         </div>
 
-                        <div className="lg:w-3/12 md:w-4/12 lg:h-[30rem] md:h-[20rem] h-[30rem] w-full overflow-hidden p-3">
-                            <div className="h-2/6 md:hidden animate-fadeIn">
+                        <div className="lg:w-3/12 md:w-4/12 lg:h-[30rem] md:h-[20rem] h-[30rem] w-full overflow-hidden">
+                            <div className="md:h-[5rem] h-[6rem] px-3 pt-3">
+                                <div className="grid grid-cols-3 animate-fadeIn">
+                                    <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded p-2'>
+                                        <i className="bi-eye-fill text-lg mb-2"/>
+                                        <div className='text-xs'>{GetPostDetail.View}</div>
+                                    </button>
+                                    <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded p-2'>
+                                        <i className="bi-flag-fill text-lg mb-2"/>
+                                        <div className='text-xs'>Report</div>
+                                    </button>
+                                    <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded p-2'>
+                                        <i className="bi-cloud-arrow-down-fill text-lg mb-2"/>
+                                        <div className='text-xs'>Download</div>
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="h-[8rem] md:hidden animate-fadeIn px-3">
                                 <div className='flex mb-5'>
                                     <div style={{ backgroundImage: `url(${GetPostDetail.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
                                     <div className='w-10/12 self-start'>
@@ -49,28 +69,16 @@ const PostDetail = () => {
                                 </div>
                             </div>
 
-                            <div className="h-1/6 grid grid-cols-3 animate-fadeIn">
-                                <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded'>
-                                    <i className="bi-eye-fill text-xl mb-2"/>
-                                    <div className='text-xs'>{GetPostDetail.View}</div>
-                                </button>
-                                <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded'>
-                                    <i className="bi-flag-fill text-xl mb-2"/>
-                                    <div className='text-xs'>Report</div>
-                                </button>
-                                <button className='dark:hover:bg-zinc-700/50 text-zinc-500 rounded'>
-                                    <i className="bi-cloud-arrow-down-fill text-xl mb-2"/>
-                                    <div className='text-xs'>Download</div>
-                                </button>
+                            <div className="h-[2rem] text-zinc-500 flex justify-between px-5">
+                                <div className="text-right text-xs self-center font-medium">Similar Post</div>
+                                <i className="bi-list text-xl self-center"></i>
                             </div>
                             
                             {GetSimilar.length != 0 && (
-                                <div className="md:h-5/6 h-3/6 overflow-y-scroll overflow-x-hidden animate-fadeIn">
-                                    <div className="text-zinc-500 text-right text-xs font-medium pt-5 pb-3 px-3">Similar Post</div>
-                                    
+                                <div className="lg:h-[22rem] md:h-[12rem] h-[13rem] overflow-y-scroll overflow-x-hidden px-3 animate-fadeIn">
                                     {GetSimilar.map(doc => {
                                         return (
-                                            <Link to={`/${doc.Id}`} key={doc.Id} className="dark:hover:bg-zinc-700/50 text-zinc-500 flex rounded p-3" onClick={OnTop}>
+                                            <Link to={`/${doc.Id}`} key={doc.Id} className="dark:hover:bg-zinc-700/50 text-zinc-500 flex rounded p-3" Title={doc.Title} onClick={OnTop}>
                                                 <div style={{ backgroundImage: `url(${doc.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
                                                 <div className='md:w-8/12 w-10/12'>
                                                     <div className='text-zinc-500 self-center whitespace-nowrap text-sm text-ellipsis overflow-hidden'>{doc.Title}</div>
@@ -92,24 +100,24 @@ const PostDetail = () => {
                     {GetPostDetail && (
                         <div className="rounded lg:w-9/12 md:w-8/12 w-full h-full md:block hidden">
                             <div className='rounded overflow-hidden animate-fadeIn'>
-                                    <div className='flex mb-5'>
-                                        <div style={{ backgroundImage: `url(${GetPostDetail.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
-                                        <div className='w-10/12 self-start'>
-                                            <div className='text-zinc-500 whitespace-nowrap font-medium text-ellipsis overflow-hidden'>{GetPostDetail.Title}</div>
-                                            <div className="text-zinc-500 flex text-xs">
-                                                <i className="bi-clock-fill self-center mr-2"></i>
-                                                <div className="self-center mr-3">{GetPostDetail.Release.toDate().toLocaleDateString('sv')}</div>
-                                            </div>
+                                <div className='flex mb-5'>
+                                    <div style={{ backgroundImage: `url(${GetPostDetail.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
+                                    <div className='w-10/12 self-start'>
+                                        <div className='text-zinc-500 whitespace-nowrap font-medium text-ellipsis overflow-hidden'>{GetPostDetail.Title}</div>
+                                        <div className="text-zinc-500 flex text-xs">
+                                            <i className="bi-clock-fill self-center mr-2"></i>
+                                            <div className="self-center mr-3">{GetPostDetail.Release.toDate().toLocaleDateString('sv')}</div>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div className='flex flex-wrap gap-1'>
-                                        {GetPostDetail.Genre.sort().map(doc => {
-                                            return(
-                                                <button key={doc} className="dark:hover:bg-zinc-700/50 text-zinc-500 border-zinc-700/50 border rounded-sm text-xs px-3 py-1">{doc}</button>
-                                            )
-                                        })}
-                                    </div>
+                                <div className='flex flex-wrap gap-1'>
+                                    {GetPostDetail.Genre.sort().map(doc => {
+                                        return(
+                                            <button key={doc} className="dark:hover:bg-zinc-700/50 text-zinc-500 border-zinc-700/50 border rounded-sm text-xs px-3 py-1">{doc}</button>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
                     )}
@@ -130,15 +138,15 @@ const PostDetail = () => {
                             </button>
                         </div>
 
-                        {GetSimilar.map(doc => {
+                        {DataNewRelease.map(doc => {
                             return (
-                                <Link to={`/${doc.Id}`} key={doc.Id} className="dark:hover:bg-zinc-700/50 text-zinc-500 flex rounded p-3" onClick={OnTop}>
-                                    <div style={{ backgroundImage: `url(${GetPostDetail.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
+                                <Link to={`/${doc.Id}`} key={doc.Id} className="dark:hover:bg-zinc-700/50 text-zinc-500 flex rounded p-3" Title={doc.Title} onClick={OnTop}>
+                                    <div style={{ backgroundImage: `url(${doc.Cover})` }} className='bg-no-repeat bg-cover bg-center rounded-full self-start w-10 h-10 mr-3'></div>
                                     <div className='md:w-8/12 w-10/12'>
-                                        <div className='text-zinc-500 self-center whitespace-nowrap text-sm text-ellipsis overflow-hidden'>{GetPostDetail.Title}</div>
+                                        <div className='text-zinc-500 self-center whitespace-nowrap text-sm text-ellipsis overflow-hidden'>{doc.Title}</div>
                                         <div className="text-zinc-500 flex text-xs">
                                             <i className="bi-eye-fill self-center mr-2"></i>
-                                            <div className="self-center mr-3">{GetPostDetail.View}</div>
+                                            <div className="self-center mr-3">{doc.View}</div>
                                         </div>
                                     </div>
                                 </Link>
